d43bc7627113d3659e61d2dbf5b263fa73c9d870
Tim McFadden <mtik00@users.noreply.github.com>
2025-10-05 18:18:55 -0600

    feat: more lenient on security level and money

--- src/v1-hack.ts
+++ src/v1-hack.ts
@@ -4,33 +4,20 @@ import { NS } from "@ns";
 export async function main(ns: NS) {
     const options = ns.flags([
         ['target', 'n00dles'],
+        ['maxMoneyFactor', 0.75],
+        ['minSecurityAdjust', 5],
     ]);
     const target = options.target.toString()
 
-    if (target === "home" || target.toString().startsWith("pserv")) {
-        ns.tprint("ERROR: You cannot hack your own server:", target)
-        return
-    }
+    const moneyThresh = ns.getServerMaxMoney(target) * parseFloat(options.maxMoneyFactor.toString())
+    const securityThresh = ns.getServerMinSecurityLevel(target) + parseInt(options.minSecurityAdjust.toString())
 
-    ns.tprintf("hacking server: %s from %s", target, ns.getHostname());
-
-    const moneyThresh = ns.getServerMaxMoney(target);
-
-    // Defines the minimum security level the target server can
-    // have. If the target's security level is higher than this,
-    // we'll weaken it before doing anything else
-    const securityThresh = ns.getServerMinSecurityLevel(target);
-
-    // Infinite loop that continously hacks/grows/weakens the target server
     while (true) {
         if (ns.getServerSecurityLevel(target) > securityThresh) {
-            // If the server's security level is above our threshold, weaken it
             await ns.weaken(target);
         } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-            // If the server's money is less than our threshold, grow it
             await ns.grow(target);
         } else {
-            // Otherwise, hack it
             await ns.hack(target);
         }
     }
