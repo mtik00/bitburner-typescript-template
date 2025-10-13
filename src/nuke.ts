4cb8311f024915886ac21212b4dc4ed437881bc0
Tim McFadden <mtik00@users.noreply.github.com>
2025-10-13 15:30:10 -0600

    refactor: all nuke.js without flags

--- src/nuke.ts
+++ src/nuke.ts
@@ -9,12 +9,23 @@ const argsSchema = [
 
 export async function main(ns: NS) {
     const options = ns.flags(argsSchema);
+    let target = options.target.toString()
 
-    if (options.target.length === 0) {
+    if ((target.length === 0) && (options._.length > 0)) {
+        target = options._[0]
+    } else if (target.length === 0) {
         ns.tprint("USAGE: nuke --target <target>");
         return
     }
 
-    openServer(ns, options.target);
-    ns.nuke(options.target);
+    openServer(ns, target)
+    ns.nuke(target)
+
+    ns.tprintf("Opened and NUKE'd %s", target)
+
+    if (ns.getServer(target).backdoorInstalled) {
+        ns.tprint("... backdoor already installed")
+    } else {
+        ns.tprint("... backdoor NOT installed")
+    }
 }
