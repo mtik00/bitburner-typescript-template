d61bde3ec847307a86294cfeae2a6e6a7c6f6091
Tim McFadden <mtik00@users.noreply.github.com>
2025-10-13 12:07:39 -0600

    fix: deprecated functions in monitor.js

--- src/monitor.js
+++ src/monitor.js
@@ -1,4 +1,6 @@
-export async function main(ns) {
+import { NS } from "@ns";
+
+export async function main(ns: NS) {
     const flags = ns.flags([
         ['refreshrate', 200],
         ['help', false],
@@ -21,7 +23,7 @@ export async function main(ns) {
         const sec = ns.getServerSecurityLevel(server);
         ns.clearLog(server);
         ns.print(`${server}:`);
-        ns.print(` $_______: ${ns.nFormat(money, "$0.000a")} / ${ns.nFormat(maxMoney, "$0.000a")} (${(money / maxMoney * 100).toFixed(2)}%)`);
+        ns.print(` $_______: $${ns.formatNumber(money, 2)} / $${ns.formatNumber(maxMoney, 2)} (${(money / maxMoney * 100).toFixed(2)}%)`);
         ns.print(` security: +${(sec - minSec).toFixed(2)}`);
         ns.print(` hack____: ${ns.tFormat(ns.getHackTime(server))} (t=${Math.ceil(ns.hackAnalyzeThreads(server, money))})`);
         ns.print(` grow____: ${ns.tFormat(ns.getGrowTime(server))} (t=${Math.ceil(ns.growthAnalyze(server, maxMoney / money))})`);
