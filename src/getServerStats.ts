5452f617f5322b15a6882938bdcd164d29a77cdc
Tim McFadden <mtik00@users.noreply.github.com>
2025-10-19 10:37:36 -0600

    feat: adding target to getServerStats

--- src/getServerStats.ts
+++ src/getServerStats.ts
@@ -1,6 +1,16 @@
+// @ts-nocheck
+import { NS, ScriptArg } from '@ns'
 import { scanAllServers } from './helpers.js'
 
-function get_action(ns, host) {
+function getTargetArg(args: ScriptArg[]): ScriptArg | string {
+    const targetIndex = args.indexOf('--target');
+    if (targetIndex !== -1 && targetIndex < args.length - 1) {
+        return args[targetIndex + 1].toString()
+    }
+    return "??"
+}
+
+function get_action(ns: NS, host: string) {
     /*
     Gets the first action in the list and returns it.
     */
@@ -8,36 +18,38 @@ function get_action(ns, host) {
     if (actions.length == 0) {
         return null
     }
-    return actions[0].filename.replace("scripts/", "").replace(".js", "")
+    const filename = actions[0].filename.replace("scripts/", "").replace(".js", "")
+    const target = getTargetArg(actions[0].args)
+    return `${filename}(${target})`
 }
 
-function pad_str(string, len) {
+function pad_str(str: string, len: number) {
     /*
     Prepends the requested padding to the string.
     */
     var pad = "                      "
-    return String(pad + string).slice(-len)
+    return String(pad + str).slice(-len)
 }
 
-function get_server_data(ns, server) {
+function get_server_data(ns: NS, hostname: string) {
     /*
     Creates the info text for each server. Currently gets money, security, and ram.
     NOTE: ns.getServer() can return a server object and obtain all of the necessary properties.
     However, ns.getServer() costs 2GB, which doubles the RAM requirement for this script.
     */
-    var moneyAvailable = ns.getServerMoneyAvailable(server)
-    var moneyMax = ns.getServerMaxMoney(server)
-    var securityLvl = ns.getServerSecurityLevel(server)
-    var securityMin = ns.getServerMinSecurityLevel(server)
-    var ram = ns.getServerMaxRam(server)
-    return `${pad_str(server, 17)}` +
+    var moneyAvailable = ns.getServerMoneyAvailable(hostname).toString()
+    var moneyMax = ns.getServerMaxMoney(hostname)
+    var securityLvl = ns.getServerSecurityLevel(hostname)
+    var securityMin = ns.getServerMinSecurityLevel(hostname)
+    var ram = ns.getServerMaxRam(hostname)
+    return `${pad_str(hostname, 17)}` +
         ` money:${pad_str(parseInt(moneyAvailable), 12)}/${pad_str(parseInt(moneyMax), 12)}(${pad_str((moneyAvailable / moneyMax).toFixed(2), 4)})` +
         ` security:${pad_str(securityLvl.toFixed(2), 6)}(${pad_str(securityMin, 2)})` +
         ` RAM:${pad_str(parseInt(ram), 4)}` +
-        ` Action:${pad_str(get_action(ns, server), 7)}`
+        ` Action:${pad_str(get_action(ns, hostname), 20)}`
 }
 
-export async function main(ns) {
+export async function main(ns: NS) {
     var servers = scanAllServers(ns, false)
     var stats = {}
 
