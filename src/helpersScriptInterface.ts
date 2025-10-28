f1e11457f535e457d249fb1d7cb6e77a83fca9b7
Tim McFadden <mtik00@users.noreply.github.com>
2025-10-27 19:01:11 -0600

    feat: adding backdoorServers to helpers

--- src/helpersScriptInterface.ts
+++ src/helpersScriptInterface.ts
@@ -5,6 +5,8 @@ import { NS, Server, ProcessInfo, ScriptArg } from "@ns";
 
 export const RAM = [512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]
 export const HOME_RAM_KEEP = 32
+export const backdoorServers = new Set(["CSEC", "I.I.I.I", "avmnite-02h", "run4theh111z", "clarkinc", "nwo", "omnitek", "fulcrumtech", "fulcrumassets", "w0r1d_d43m0n"]);
+
 
 /**
  * A generic function for creating autocomplete data.
