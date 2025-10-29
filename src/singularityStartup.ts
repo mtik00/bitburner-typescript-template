62f2197d5b0a2efafbd94823e6200a00fab75df3
Tim McFadden <mtik00@users.noreply.github.com>
2025-10-29 17:58:58 -0600

    refactor: try more intelligent backdoor

--- src/singularityStartup.ts
+++ src/singularityStartup.ts
@@ -2,8 +2,8 @@ import { NS } from "@ns";
 import { backdoorServers, waitForPID } from "./helpersScriptInterface";
 
 export async function main(ns: NS): Promise<void> {
-
-    const myMoney = ns.getPlayer().money
+    const player = ns.getPlayer()
+    const myMoney = player.money
     if (!ns.hasTorRouter() && myMoney > 1000000) {
         ns.singularity.purchaseTor()
     }
@@ -13,9 +13,14 @@ export async function main(ns: NS): Promise<void> {
         await ns.asleep(5000)
     }
 
-    for (const server of backdoorServers) {
-        if (!ns.getServer(server).backdoorInstalled) {
-            const pid = ns.exec("backdoor.js", "home", 1, server)
+    for (const hostname of backdoorServers) {
+        const server = ns.getServer(hostname)
+        const hackDifficulty = server.hackDifficulty !== undefined ? server.hackDifficulty : 1E10
+        const numOpenPortsRequired = server.numOpenPortsRequired !== undefined ? server.numOpenPortsRequired : 1E10
+        const openPortCount = server.openPortCount !== undefined ? server.openPortCount : 1E10
+
+        if (hackDifficulty <= player.skills.hacking && numOpenPortsRequired <= openPortCount) {
+            const pid = ns.exec("backdoor.js", "home", 1, hostname)
             waitForPID(ns, pid, "home")
         }
     }
