f71fd58f84adc77e05d5feda8d83a531f6093b41
Tim McFadden <mtik00@users.noreply.github.com>
2025-10-29 18:29:14 -0600

    feat: purchase darkweb programs during startup

--- src/singularityStartup.ts
+++ src/singularityStartup.ts
@@ -1,11 +1,47 @@
 import { NS } from "@ns";
 import { backdoorServers, waitForPID } from "./helpersScriptInterface";
 
+const programs = [
+    "BruteSSH.exe",
+    "FTPCrack.exe",
+    "relaySMTP.exe",
+    "HTTPWorm.exe",
+    "SQLInject.exe",
+    "ServerProfiler.exe",
+    "DeepscanV1.exe",
+    "DeepscanV2.exe",
+    "AutoLink.exe",
+    "Formulas.exe",
+]
+
 export async function main(ns: NS): Promise<void> {
     const player = ns.getPlayer()
     const myMoney = player.money
     if (!ns.hasTorRouter() && myMoney > 1000000) {
-        ns.singularity.purchaseTor()
+        const success = ns.singularity.purchaseTor()
+        if (!success) {
+            ns.tprint(`ERROR: Failed to purchase TOR`)
+        } else {
+            ns.tprint(`Purchased TOR`)
+        }
+    }
+
+    if (ns.hasTorRouter()) {
+        for (const program of programs) {
+            if (ns.fileExists(program)) {
+                continue
+            }
+
+            const cost = ns.singularity.getDarkwebProgramCost(program)
+            if (cost <= ns.getPlayer().money) {
+                const success = ns.singularity.purchaseProgram(program);
+                if (!success) {
+                    ns.tprint(`ERROR: Failed to purchase ${program}`)
+                } else {
+                    ns.tprint(`Purchased ${program}`)
+                }
+            }
+        }
     }
 
     while (ns.getPlayer().skills.hacking < 10) {
