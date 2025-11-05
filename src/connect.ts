825578f160cc30597504caf1a761548db364bbf0
Tim McFadden <mtik00@users.noreply.github.com>
2025-11-05 14:48:30 -0700

    chore: usage for connect.ts

--- src/connect.ts
+++ src/connect.ts
@@ -5,6 +5,8 @@ import { filterHackableServers } from "./lib/autocomplete";
 export async function main(ns: NS): Promise<void> {
     const target = ns.args[0];
     if (target === undefined) {
+        ns.tprint(`USAGE: ${ns.getScriptName()} <target>`)
+        ns.tprint("...This script will connect you to the target server")
         return
     }
 
