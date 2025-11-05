5f2cec43812d4820e2882e7430786f0bdf8cc768
Tim McFadden <mtik00@users.noreply.github.com>
2025-11-04 21:11:10 -0700

    feat: adding server hack score

--- src/getServerStats.ts
+++ src/getServerStats.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { NS } from '@ns'
-import { scanAllServers } from './helpers.js'
+import { scanAllServers } from './lib/scan'
 import { getArgValue } from './helpersScriptInterface.js'
 
 
@@ -25,22 +25,28 @@ function pad_str(str: string, len: number) {
     return String(pad + str).slice(-len)
 }
 
+function dollarsPerSecond(ns: NS, hostname: string): number {
+    return (ns.getServerMaxMoney(hostname) * ns.hackAnalyzeChance(hostname)) / (ns.getWeakenTime(hostname) + ns.getGrowTime(hostname) + ns.getHackTime(hostname))
+}
+
 function get_server_data(ns: NS, hostname: string) {
     /*
     Creates the info text for each server. Currently gets money, security, and ram.
     NOTE: ns.getServer() can return a server object and obtain all of the necessary properties.
     However, ns.getServer() costs 2GB, which doubles the RAM requirement for this script.
     */
-    var moneyAvailable = ns.getServerMoneyAvailable(hostname).toString()
-    var moneyMax = ns.getServerMaxMoney(hostname)
-    var securityLvl = ns.getServerSecurityLevel(hostname)
-    var securityMin = ns.getServerMinSecurityLevel(hostname)
-    var ram = ns.getServerMaxRam(hostname)
+    const moneyAvailable = ns.getServerMoneyAvailable(hostname).toString()
+    const moneyMax = ns.getServerMaxMoney(hostname)
+    const securityLvl = ns.getServerSecurityLevel(hostname)
+    const securityMin = ns.getServerMinSecurityLevel(hostname)
+    const ram = ns.getServerMaxRam(hostname)
+    const value = dollarsPerSecond(ns, hostname)
+
     return `${pad_str(hostname, 17)}` +
         ` money:${pad_str(parseInt(moneyAvailable), 12)}/${pad_str(parseInt(moneyMax), 12)}(${pad_str((moneyAvailable / moneyMax).toFixed(2), 4)})` +
         ` security:${pad_str(securityLvl.toFixed(2), 6)}(${pad_str(securityMin, 2)})` +
         ` RAM:${pad_str(parseInt(ram), 4)}` +
-        ` Action: ${get_action(ns, hostname)}`
+        ` $/s: ${ns.formatNumber(value, 2)}`
 }
 
 export async function main(ns: NS) {
@@ -54,7 +60,8 @@ export async function main(ns: NS) {
 
     // Sort each server based on how much money it holds.
     var keys = Object.keys(stats)
-    keys.sort((a, b) => ns.getServerMaxMoney(a) - ns.getServerMaxMoney(b))
+    // keys.sort((a, b) => ns.getServerMaxMoney(a) - ns.getServerMaxMoney(b))
+    keys.sort((a, b) => dollarsPerSecond(ns, a) - dollarsPerSecond(ns, b))
 
     for (var i in keys) {
         var key = keys[i]
