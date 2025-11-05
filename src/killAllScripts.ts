f4a929fc262aed2ae13fcdaab0ea15a2b1e9b2b3
Tim McFadden <mtik00@users.noreply.github.com>
2025-11-05 08:41:14 -0700

    fix: dont kill the killall script

--- src/killAllScripts.ts
+++ src/killAllScripts.ts
@@ -1,8 +1,9 @@
+import { NS } from "@ns";
 import { scanAllServers } from './helpers.js'
 
 // the purpose of cascade kill is to kill all scripts running on any server in the game
 // but saving the host that you run it on for last (so that it doesn't kill itself prematurely)
-export async function main(ns) {
+export async function main(ns: NS) {
     var startingNode = ns.getHostname();
     const serverList = scanAllServers(ns);
 
@@ -13,7 +14,7 @@ export async function main(ns) {
             continue;
 
         // skip if not running anything
-        if (ns.ps(server) === 0)
+        if (ns.ps(server).length === 0)
             continue;
 
         // kill all scripts
@@ -26,7 +27,7 @@ export async function main(ns) {
         if (server == startingNode)
             continue;
         // idle until they're dead, this is to avoid killing the cascade before it's finished.
-        while (ns.ps(server) > 0) {
+        while (ns.ps(server).length > 0) {
             await ns.sleep(20);
         }
         // Remove script files the daemon would have copied over (in case we update the source)
@@ -34,6 +35,6 @@ export async function main(ns) {
             ns.rm(file, server)
     }
 
-    // wait to kill these. This kills itself, obviously.
-    ns.killall(startingNode);
+    // wait to kill these. This will not kill this script, just everything else.
+    ns.killall(startingNode, true);
 }
