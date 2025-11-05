944a785228942b652244877def7ab69754d010a8
Tim McFadden <mtik00@users.noreply.github.com>
2025-11-05 14:53:15 -0700

    ci: no-check contract solver

--- src/contracts/solve-contract.ts
+++ src/contracts/solve-contract.ts
@@ -1,5 +1,7 @@
-/** @param {NS} ns **/
-export async function main(ns) {
+// @ts-nocheck
+import { NS } from "@ns";
+
+export async function main(ns: NS) {
     solveContract(ns, ns.args[0], ns.args[1], 1);
 }
 
