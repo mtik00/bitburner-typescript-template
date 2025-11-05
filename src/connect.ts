19729b3cc814ad4c0bec85f7c9e3191f75de04be
Tim McFadden <mtik00@users.noreply.github.com>
2025-11-05 08:30:54 -0700

    feat: adding connect script

---
+++ src/connect.ts
@@ -0,0 +1,35 @@
+import { NS } from "@ns";
+import { findPath } from "/helpers";
+import { filterHackableServers } from "/helpersScriptInterface";
+
+export async function main(ns: NS): Promise<void> {
+    const target = ns.args[0];
+    if (target === undefined) {
+        return
+    }
+
+    const hackSkill = ns.getHackingLevel()
+    const hackRequired = ns.getServerRequiredHackingLevel(target.toString())
+    if (hackSkill < hackRequired) {
+        ns.print(`can't hack ${target} yet`)
+        return
+    }
+
+    const [results, isFound] = findPath(ns, target.toString(), "home", [], [], false)
+    if (!isFound) {
+        ns.tprintf("%s not found", target)
+        return
+    }
+
+    ns.tprintf("Connecting to %s though: %s", target, results)
+
+    for (const server of results) {
+        ns.tprint("...connecting to ", server)
+        await ns.singularity.connect(server)
+    }
+    ns.tprint("...done")
+}
+
+export function autocomplete(data: any, args: any) {
+    return filterHackableServers(data);
+}
